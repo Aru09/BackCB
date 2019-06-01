7d0d6d5283de2c53b437fe0bc8dccc2098556078
Aru09 <42914363+Aru09@users.noreply.github.com>
2019-06-01 15:57:48 +0600

    Update generic.py

--- api/views/generic.py
+++ api/views/generic.py
@@ -1,8 +1,6 @@
 from api import models
-from api.models import Review, ReviewerMetadata, Company
+from api.models import Review, Company
 from api import serializers
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
 from api.serializers import CompanySerializer, ReviewSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
@@ -19,43 +17,3 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = models.Company.objects.all()
     serializer_class = serializers.CompanySerializer
-
-
-    # def list(self, request):
-    #     queryset = Review.objects.all()
-    #     serializer = ReviewSerializer(queryset, many=True)
-    #     return Response(serializer.data)
-    #
-    # def retrieve(self, request, pk=None):
-    #     queryset = Review.objects.all()
-    #     user = get_object_or_404(queryset, pk=pk)
-    #     serializer = ReviewSerializer(user)
-    #     return Response(serializer.data)
-
-
-'''class ReviewList(generics.ListCreateAPIView):
-    permission_classes = IsAuthenticated
-    serializer_class =ReviewSerializers
-
-    def get_queryset(self):
-        return Review.objects.filter(created_by=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)'''
-
-
-'''class ReviewerViewSet(viewsets.ModelViewSet):
-    permission_classes = IsAuthenticated
-    # queryset = models.Review.objects.all()
-    serializer_class = serializers.ReviewSerializer
-
-    def list(self, request):
-        queryset = ReviewerMetadata.objects.all()
-        serializer = ReviewerMetadataSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = ReviewerMetadata.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = ReviewerMetadataSerializer(user)
-        return Response(serializer.data)'''
